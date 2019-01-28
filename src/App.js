0479b19fe1042623367a5675c80c1790e40e3810
Nikiya Simpson <nikiyasimpson@gmail.com>
2019-01-27 21:53:30 -0600

    Add options for questions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import question from './Question'
+import Question from './Question'
 
 class App extends Component {
 
 	  constructor() {
       super();
       this.state = {
-        
+
+				questions : [
+
+	      { number: 1,
+	      	question: 'Are you currently in a relationship?',
+	      	options: ['Yes','No','Unsure']
+	      },
+
+	      { number: 2,
+	      	question: 'Are you currently married?',
+	      	options: ['Yes','No','Unsure']
+	      },
+
+	      { number: 3,
+	      	question: 'Are you experiencing any of the following issues?',
+	      	options: ['Infidelity','Trust','Death of a Loved One', 'Communication','Addiction','Intimacy']
+	      },
+
+	      { number: 4,
+	      	question: 'Does your partner/spouse want counseling also?',
+	      	options: ['Yes','No','Unsure']
+	      },
+
+	      { number: 5,
+	      	question: 'Are there other family members that you would like to include in your sessions (e.g. children, parents) ?',
+	      	options: ['Yes','No','Unsure']
+	      },
+
+	      { number: 6,
+	      	question: 'Are you dealing with a lot of pain in your relationship?',
+	      	options: ['Yes','No','Unsure']
+	      },
+
+	      { number: 7,
+	      	question: 'We prefer to have our counseling sessions ...',
+	      	options: ['In-Office','Phone','Online','At our home']
+	      },
+
+	      { number: 8,
+	      	question: 'Both of us are ready to end this relationship?',
+	      	options: ['Yes','No','Unsure']
+	      },
+
+	      { number: 9,
+	      	question: 'Would your partner/spouse be able to go to counseling with you?',
+	      	options: ['Yes','No','Unsure']
+	      }
+
+			],
+
           answers: [],
-        
+
 
           updateSuperState: obj => {
             this.setState(obj);
           }
         };
 
-      const questions = [ 
+      const questions = [
 
       { number: 1,
       	question: 'Are you currently in a relationship?',
@@ -30,7 +79,7 @@ class App extends Component {
       },
 
       { number: 3,
-      	question: 'Are you experiencing any of the following issues?'
+      	question: 'Are you experiencing any of the following issues?',
       	options: ['Infidelity','Trust','Death of a Loved One', 'Communication','Addiction','Intimacy']
       },
 
@@ -50,7 +99,7 @@ class App extends Component {
       },
 
       { number: 7,
-      	question: 'We prefer to have our counseling sessions ...'
+      	question: 'We prefer to have our counseling sessions ...',
       	options: ['In-Office','Phone','Online','At our home']
       },
 
@@ -65,16 +114,19 @@ class App extends Component {
       }
 
       ]
-      
+
     }
 
 
   render() {
+
+
     return (
       <div className="App">
           <header className="App-header">
-            <h1>What Type of Counseling Do You Need?</h1>
-       		</header>
+          </header>
+
+       		<Question question ={this.state.questions[0]} />
 
        		<footer>
             	<p>Nikiya M. Simpson, Arkansas Relationship Counseling Center</p>
--- src/Question.js
+++ src/Question.js
@@ -1,16 +1,22 @@
-/*This component is for the book*/
+
 import React, { Component } from 'react'
 
+
 class Question extends Component {
+  MakeItem(X) {
+               return <option>{X}</option>;
+           };
+
 
   render() {
     /* Renders question component */
 
+
      return (
           <div className="question">
-            {this.props.question}
+            {this.props.question.question}
             <div className="options">
-              {this.props.answers}
+            <select>{this.props.question.options.map(this.MakeItem)}</select>;
             </div>
           </div>
      )
