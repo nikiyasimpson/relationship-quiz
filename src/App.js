234758e067d9002d02ff09f9dbf7a3b23a2d1669
Nikiya Simpson <nikiyasimpson@gmail.com>
2019-01-29 20:47:10 -0600

    Show Questions Function

--- src/App.js
+++ src/App.js
@@ -13,52 +13,60 @@ class App extends Component {
 
 	      { number: 1,
 	      	question: 'Are you currently in a relationship?',
-	      	options: ['Yes','No','Unsure']
+	      	options: ['Yes','No','Unsure'],
+					answer: ''
 	      },
 
 	      { number: 2,
 	      	question: 'Are you currently married?',
-	      	options: ['Yes','No','Unsure']
+	      	options: ['Yes','No','Unsure'],
+					answer: ''
+
 	      },
 
 	      { number: 3,
 	      	question: 'Are you experiencing any of the following issues?',
-	      	options: ['Infidelity','Trust','Death of a Loved One', 'Communication','Addiction','Intimacy']
+	      	options: ['Infidelity','Trust','Death of a Loved One', 'Communication','Addiction','Intimacy'],
+					answer: ''
 	      },
 
 	      { number: 4,
 	      	question: 'Does your partner/spouse want counseling also?',
-	      	options: ['Yes','No','Unsure']
+	      	options: ['Yes','No','Unsure'],
+					answer: ''
 	      },
 
 	      { number: 5,
 	      	question: 'Are there other family members that you would like to include in your sessions (e.g. children, parents) ?',
-	      	options: ['Yes','No','Unsure']
+	      	options: ['Yes','No','Unsure'],
+					answer: ''
 	      },
 
 	      { number: 6,
 	      	question: 'Are you dealing with a lot of pain in your relationship?',
-	      	options: ['Yes','No','Unsure']
+	      	options: ['Yes','No','Unsure'],
+					answer: ''
 	      },
 
 	      { number: 7,
 	      	question: 'We prefer to have our counseling sessions ...',
-	      	options: ['In-Office','Phone','Online','At our home']
+	      	options: ['In-Office','Phone','Online','At our home'],
+					answer: ''
 	      },
 
 	      { number: 8,
 	      	question: 'Both of us are ready to end this relationship?',
-	      	options: ['Yes','No','Unsure']
+	      	options: ['Yes','No','Unsure'],
+					answer: ''
 	      },
 
 	      { number: 9,
 	      	question: 'Would your partner/spouse be able to go to counseling with you?',
-	      	options: ['Yes','No','Unsure']
+	      	options: ['Yes','No','Unsure'],
+					answer: ''
 	      }
 
 			],
-
-          answers: [],
 
 
           updateSuperState: obj => {
@@ -126,7 +134,7 @@ class App extends Component {
           <header className="App-header">
           </header>
 
-       		<Question question ={this.state.questions[0]} />
+       		<Question questions ={this.state.questions} />
 
        		<footer>
             	<p>Nikiya M. Simpson, Arkansas Relationship Counseling Center</p>
--- src/Question.js
+++ src/Question.js
@@ -7,16 +7,42 @@ class Question extends Component {
                return <option>{X}</option>;
            };
 
+  ShowQuestion() {
+    if(this.props.questions[0].answer === '') {
+      return this.props.questions[0];
+    }
+    if(this.props.questions[0].answer === 'Yes') {
+      return this.props.questions[1];
+    }
+
+  }
+
+  /*NextQuestion()
+  {
+    if(question.no === 1) {
+      if (question.answer == "") {
+        return question;
+      }
+      else if (question.answer === "No") {
+      return this.props.questions[2];
+      }
+      else if(question.answer ==="Yes" || question.answer=="Unsure") {
+      return this.props.questions[1];
+      }
+    }
+
+  };*/
+
 
   render() {
     /* Renders question component */
-
+    let firstquestion = this.ShowQuestion();
 
      return (
           <div className="question">
-            {this.props.question.question}
+            {firstquestion.question}
             <div className="options">
-            <select>{this.props.question.options.map(this.MakeItem)}</select>;
+            <select>{firstquestion.options.map(this.MakeItem)}</select>
             </div>
           </div>
      )
